8174593ab85cdc050ba538b4496201b740fd8b7b
Tom Strausbaugh <tstrausbaugh@straustech.net>
2023-12-12 23:10:59 -0500

    Update Program.cs

--- Day_8/Day_8/Program.cs
+++ Day_8/Day_8/Program.cs
@@ -2,51 +2,123 @@
 const string BEGIN = "AAA";
 const string END = "ZZZ";
 
-var moves = 0;
-var lines = await ReadData();
-var nodes = new Dictionary<string, Node>();
-var directions = lines[0].ToArray();
 
-foreach (var line in lines.Skip(2))
+await PartTwo();
+await PartOne();
+
+async Task PartTwo()
 {
-	var parsedLine = line.Split(" = ");
-	var key = parsedLine[0];
-	var node = parsedLine[1].Replace("(", "").Replace(")", "").Split(", ");
-	var left = node[0];
-	var right = node[1];
-	
-	nodes.Add(key, new Node(left, right));
-}
+	var moves = 0;
+	var lines = await ReadData();
+	var nodes = new Dictionary<string, Node>();
+	var directions = lines[0].ToArray();
 
-TraverseNodes();
-
-Console.WriteLine($"Number of Moves needed is {moves}");
-
-void TraverseNodes()
-{
-	var nodeKey = BEGIN;
-	var direction = directions[0] == 'L' ? 0 : 1;
-	
-	while (true)
+	foreach (var line in lines.Skip(2))
 	{
-		var node = nodes[nodeKey];
+		var parsedLine = line.Split(" = ");
+		var key = parsedLine[0];
+		var node = parsedLine[1].Replace("(", "").Replace(")", "").Split(", ");
+		var left = node[0];
+		var right = node[1];
+	
+		nodes.Add(key, new Node(left, right));
+	}
 
-		moves++;
+	TraverseNodes();
+
+	Console.WriteLine($"Number of Moves needed is {moves}");
+
+	void TraverseNodes()
+	{
+		var startingNodes = new string[nodes.Count(n => n.Key.EndsWith('A'))];
+		startingNodes = nodes
+			.Where(n => n.Key.EndsWith('A'))
+			.Select(n => n.Key)
+			.ToArray();
 		
-		if (node.LeftRight[direction] == END)
-			break;
+		//var nodeKey = BEGIN;
+		var direction = directions[0] == 'L' ? 0 : 1;
+		var tempMoves = 0;
+	
+		while (true)
+		{
+			Parallel.For(0, startingNodes.Length, i =>
+			{
+				startingNodes[i] = nodes[startingNodes[i]].LeftRight[direction];
+			});
+				
+			moves++;
+			tempMoves++;
+		
+			if (startingNodes.All(n => n.EndsWith('Z')))
+				break;
 
-		nodeKey = node.LeftRight[direction];
-		direction = directions[moves % directions.Length] == 'L' ? 0 : 1;
+			if (tempMoves >= directions.Length)
+				tempMoves = 0;
+			
+			direction = directions[tempMoves] == 'L' ? 0 : 1;
+
+		}
+	}
+
+	async Task<string[]> ReadData()
+	{
+		var lines = await File.ReadAllLinesAsync(FILE_NAME);
+
+		return lines;
 	}
 }
 
-async Task<string[]> ReadData()
+async Task PartOne()
 {
-	var lines = await File.ReadAllLinesAsync(FILE_NAME);
+	var moves = 0;
+	var lines = await ReadData();
+	var nodes = new Dictionary<string, Node>();
+	var directions = lines[0].ToArray();
 
-	return lines;
+	foreach (var line in lines.Skip(2))
+	{
+		var parsedLine = line.Split(" = ");
+		var key = parsedLine[0];
+		var node = parsedLine[1].Replace("(", "").Replace(")", "").Split(", ");
+		var left = node[0];
+		var right = node[1];
+	
+		nodes.Add(key, new Node(left, right));
+	}
+
+	TraverseNodes();
+
+	Console.WriteLine($"Number of Moves needed is {moves}");
+
+	void TraverseNodes()
+	{
+		var nodeKey = BEGIN;
+		var direction = directions[0] == 'L' ? 0 : 1;
+	
+		while (true)
+		{
+			var node = nodes[nodeKey];
+
+			moves++;
+		
+			if (node.LeftRight[direction] == END)
+				break;
+
+			nodeKey = node.LeftRight[direction];
+			direction = directions[moves % directions.Length] == 'L' ? 0 : 1;
+		}
+	}
+
+	async Task<string[]> ReadData()
+	{
+		var lines = await File.ReadAllLinesAsync(FILE_NAME);
+
+		return lines;
+	}
 }
+
+
 
 struct Node
 {
